bdc9847c80765063827b1c858d284c45881333ea
Ridoy Mondol <ridoymondol140@gmail.com>
2024-12-13 14:09:02 +0600

    improved the message notification creation logic

--- src/utilities/misc/shouldCreateNotification.ts
+++ src/utilities/misc/shouldCreateNotification.ts
@@ -1,3 +1,44 @@
+// import { prisma } from "@/prisma/client";
+
+// export const shouldCreateNotification = async (sender: string, recipient: string) => {
+//     const lastNotification = await prisma.notification.findFirst({
+//         where: {
+//             type: "message",
+//             user: {
+//                 username: recipient,
+//             },
+//             content: {
+//                 contains: sender,
+//             },
+//         },
+//         orderBy: {
+//             createdAt: "desc",
+//         },
+//     });
+
+//     if (!lastNotification) return true;
+
+//     const currentTime = Date.now();
+//     const lastNotificationTime = new Date(lastNotification.createdAt).getTime();
+//     const oneHour = 60 * 60 * 1000;
+
+//     if (currentTime - lastNotificationTime >= oneHour) {
+//         return true;
+//     }
+
+//     return false;
+// };
+
+
+
+
+
+
+
+
+
+
+
 import { prisma } from "@/prisma/client";
 
 export const shouldCreateNotification = async (sender: string, recipient: string) => {
@@ -16,17 +57,15 @@ export const shouldCreateNotification = async (sender: string, recipient: string
         },
     });
 
+    // If no notification exists, allow creation of a new notification
     if (!lastNotification) return true;
 
-    const currentTime = Date.now();
-    const lastNotificationTime = new Date(lastNotification.createdAt).getTime();
-    const oneHour = 60 * 60 * 1000;
-
-    if (currentTime - lastNotificationTime >= oneHour) {
+    // If the last notification is marked as read, allow creation of a new notification
+    if (lastNotification.isRead) {
         return true;
     }
 
+    // If the last notification is unread, don't create a new notification
     return false;
 };
 
-// this function determines if enough time is passed to create notification while messaging

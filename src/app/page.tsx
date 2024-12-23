cd9935aef1190dfed0cd132808477d3c3566e4eb
Ridoy Mondol <ridoymondol140@gmail.com>
2024-12-23 11:49:03 +0600

    debugging redirect url

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -71,7 +71,7 @@ export default function RootPage() {
             const { error } = await supabase.auth.signInWithOAuth({
                 provider: "google",
                 options: {
-                    redirectTo: `${process.env.NEXT_PUBLIC_HOST_URL}/auth/google/callback`,
+                    redirectTo: `https://snip-mu.vercel.app/auth/google/callback`,
                 },
             });
 
